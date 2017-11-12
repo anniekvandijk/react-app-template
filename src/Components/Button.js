90e1044506073481b4362447fac897d229bfb474
Anniek van Dijk <anniek@animundo.nl>
2017-11-12 10:46:10 +0100

    Initial commit

---
+++ src/Components/Button.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Button extends Component {
+    render() {
+        return (
+            <Button
+                className="button"
+                text="Button"
+            />
+        );
+    }
+}
+
+export default Button;
