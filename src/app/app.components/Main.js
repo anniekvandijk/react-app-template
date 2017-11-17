8c3cb1017c0409b68b14b2ffb97000335db1d5d1
Anniek van Dijk <anniek@animundo.nl>
2017-11-17 07:40:10 +0100

    mock alpacaColorClasses

--- src/app/app.components/Main.js
+++ src/app/app.components/Main.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import MuiFlatButton from "../mui.components/buttons/FlatButton";
 import MuiFloatingActionButton from "../mui.components/buttons/RoundAddButton";
 import MuiRaisedButton from "../mui.components/buttons/RaisedButton";
+import AlpacaColorClasses from "./shared/AlpacaColorClasses";
 
 class Main extends Component {
     render() {
@@ -22,6 +23,7 @@ class Main extends Component {
                     onClick='test'
                     />
                 </p>
+                <AlpacaColorClasses/>
             </div>
         )
     }
--- src/app/app.components/shared/AlpacaColorClasses.js
+++ src/app/app.components/shared/AlpacaColorClasses.js
@@ -1,11 +1,53 @@
 import React, { Component } from 'react';
-import AlpacaColorClassService from '../../../services/AlpacaColorClassService';
+import Chip from 'material-ui/Chip';
+import Avatar from 'material-ui/Avatar';
+
+const styles = {
+    chip: {
+        margin: 4,
+
+    },
+    wrapper: {
+        display: 'flex',
+        flexWrap: 'wrap',
+    },
+
+};
+
+
+function handleRequestDelete() {
+    alert('You clicked the delete button.');
+}
+
+function handleTouchTap() {
+    alert('You clicked the Chip.');
+}
 
 class AlpacaColorClasses extends Component {
+
+
+
     render() {
         return (
-            <div id="colorClasses">
-
+            <div id="colorClasses" style={styles.wrapper}>
+                <Chip
+                    //onRequestDelete={handleRequestDelete}
+                    onClick={handleTouchTap}
+                    style={styles.chip}
+                >
+                    <Avatar size={32} backgroundColor='#4E342E'>
+                    </Avatar>
+                    Brown
+                </Chip>
+                <Chip
+                    //onRequestDelete={handleRequestDelete}
+                    onClick={handleTouchTap}
+                    style={styles.chip}
+                >
+                    <Avatar size={32} backgroundColor='#616161'>
+                    </Avatar>
+                    Grey
+                </Chip>
             </div>
         )
     }
